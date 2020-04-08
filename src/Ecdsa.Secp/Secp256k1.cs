06a723d6110715aa69753c086a5e337faeb52199
strmv <strmax20@gmail.com>
2020-04-08 10:28:13 +0300

    ecdsa secp256r1, secp256k1 lib added

---
+++ src/Ecdsa.Secp/Secp256k1.cs
@@ -0,0 +1,15 @@
+﻿namespace Ecdsa.Secp
+{
+    using Org.BouncyCastle.Asn1.X9;
+    using Org.BouncyCastle.Crypto.Parameters;
+
+    public class Secp256k1 : EcdsaSecp256Base
+    {
+        public Secp256k1()
+        {
+            // set curve
+            _curve = ECNamedCurveTable.GetByName("secp256k1");
+            _curveSpec = new ECDomainParameters(_curve.Curve, _curve.G, _curve.N, _curve.H);
+        }
+    }
+}
