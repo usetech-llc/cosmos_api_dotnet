c3877aeac219307a45b1c0bd1aad5ddf7ae71869
sotmorskiy <sotmorskiy@usetech.ru>
2020-04-20 10:28:46 +0700

    Output for correct bytes for signing test.

--- src/CosmosApi.Test/Client/SigningTests.cs
+++ src/CosmosApi.Test/Client/SigningTests.cs
@@ -18,7 +18,7 @@ namespace CosmosApi.Test.Client
         {
             using var client = CreateClient();
 
-            var bytes = client.GetSignBytes(new StdSignDoc()
+            var stdSignDoc = new StdSignDoc()
             {
                 Fee = new StdFee(100000, new List<Coin>()),
                 Memo = "test_memo",
@@ -37,10 +37,23 @@ namespace CosmosApi.Test.Client
                         }
                     })
                 }
-            });
+            };
+            var bytes = client.GetSignBytes(stdSignDoc);
+
+            OutputHelper.WriteLine("StdSignDoc:");
+            Dump(stdSignDoc);
+
+            OutputHelper.WriteLine("SignBytes:");
+            OutputHelper.WriteLine(bytes.ToHexString());
+            OutputHelper.WriteLine("");
+            
+            var expectedBytes = "7b226163636f756e745f6e756d626572223a223133222c22636861696e5f6964223a22746573745f636861696e222c22666565223a7b22616d6f756e74223a5b5d2c22676173223a22313030303030227d2c226d656d6f223a22746573745f6d656d6f222c226d736773223a5b7b2274797065223a22636f736d6f732d73646b2f4d736753656e64222c2276616c7565223a7b22616d6f756e74223a5b7b22616d6f756e74223a223130222c2264656e6f6d223a22666f6f636f696e227d5d2c2266726f6d5f61646472657373223a22636f736d6f733171797071787071397163756866777978222c22746f5f61646472657373223a22636f736d6f733176346e7877357774366a37227d7d5d2c2273657175656e6365223a223137227d";
+            OutputHelper.WriteLine("Expected SignBytes:");
+            OutputHelper.WriteLine(expectedBytes);
+            OutputHelper.WriteLine("");
 
             Assert.Equal( 
-                ByteArrayExtensions.ParseHexString("7b226163636f756e745f6e756d626572223a223133222c22636861696e5f6964223a22746573745f636861696e222c22666565223a7b22616d6f756e74223a5b5d2c22676173223a22313030303030227d2c226d656d6f223a22746573745f6d656d6f222c226d736773223a5b7b2274797065223a22636f736d6f732d73646b2f4d736753656e64222c2276616c7565223a7b22616d6f756e74223a5b7b22616d6f756e74223a223130222c2264656e6f6d223a22666f6f636f696e227d5d2c2266726f6d5f61646472657373223a22636f736d6f733171797071787071397163756866777978222c22746f5f61646472657373223a22636f736d6f733176346e7877357774366a37227d7d5d2c2273657175656e6365223a223137227d"),
+                ByteArrayExtensions.ParseHexString(expectedBytes),
                 bytes);
         }
     }
